fdc7300a4a4407c0e55132aefe959eb5c2471576
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-03-23 00:26:02 +0100

    Fixed broken naming convention for Api project

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,19 +1,19 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
-app.UseAuthorization();
-app.MapControllers();
+app.UseHttpsRedirection();
+
 
 app.Run();
+
