dcce12cf44e3b710ed72fa82ff97c132f3b4a01d
Alexander-Karlsson <Alexanderkarlsson3@gmail.com>
2026-03-27 13:37:37 +0100

    Added Seed Data for Product Categories

--- EF_MSSQL/Migrations/20260327123627_SeedProductCategories.Designer.cs
+++ EF_MSSQL/Migrations/20260327123627_SeedProductCategories.Designer.cs
@@ -4,6 +4,7 @@ using EF_MSSQL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF_MSSQL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327123627_SeedProductCategories")]
+    partial class SeedProductCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EF_MSSQL/Migrations/20260327123627_SeedProductCategories.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EF_MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890"), "Think different. Think iPhone.", "iPhone" },
+                    { new Guid("b2c3d4e5-f6a7-8901-bcde-f12345678901"), "Do more. Much more.", "Mac" },
+                    { new Guid("c3d4e5f6-a7b8-9012-cdef-123456789012"), "Sound that moves with you.", "AirPods" },
+                    { new Guid("d4e5f6a7-b8c9-0123-defa-234567890123"), "Thin. Powerful. Endless.", "iPad" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890"));
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("b2c3d4e5-f6a7-8901-bcde-f12345678901"));
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("c3d4e5f6-a7b8-9012-cdef-123456789012"));
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("d4e5f6a7-b8c9-0123-defa-234567890123"));
+        }
+    }
+}
